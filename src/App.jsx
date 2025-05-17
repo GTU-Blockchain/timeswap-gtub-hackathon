add09cfde40edacee4333535dc52dd2c6d04ade2
mehmetnuribasa <basamehmet394@gmail.com>
2025-05-18 01:05:56 +0300

    added profile photo page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Profile from "./pages/Profile";
 
 function App() {
     return (
@@ -7,6 +8,7 @@ function App() {
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<Home />} />
+                    <Route path="/profile" element={<Profile />} />
                 </Routes>
             </BrowserRouter>
         </>
