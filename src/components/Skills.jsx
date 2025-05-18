e7c6f6fdc776115903617c429d14f470cfea7127
umutsatir <u.satir2021@gtu.edu.tr>
2025-05-18 13:08:43 +0300

    explore.jsx conected to the contract

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,43 +1,43 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
-const Skills = () => {
-  //    Hard codded, soon to be changed
-  const skills = [
-    { name: "Python Programming", hours: 1000, reputation: 5.0 },
-    { name: "Product Design", hours: 200, reputation: 4.9 },
-    { name: "Mentorship", hours: 500, reputation: 4.8 },
-    { name: "Marketing", hours: 300, reputation: 4.7 },
-  ];
+const Skills = ({ skills }) => {
+    const navigate = useNavigate();
 
-  return (
-    <div>
-      <h2 className="text-lg font-semibold mb-4">Results</h2>
-      <ul className="space-y-4">
-        {skills.map((skill, index) => (
-          <li
-            key={index}
-            className="flex justify-between items-center dark:hover:bg-[var(--color-secondary-dark)] hover:bg-[var(--color-secondary)] transition-colors duration-200 p-4 rounded-xl"
-          >
-            <div className="flex items-center justify-center">
-              <img
-                src="https://picsum.photos/200"
-                className="h-16 w-16 rounded-full mr-6"
-              />
-              <div>
-                <div className="font-semibold">{skill.name}</div>
-                <div className="text-sm text-gray-400">
-                  {skill.hours} hours, {skill.reputation.toFixed(1)} reputation
-                </div>
-              </div>
-            </div>
-            <button className="bg-[var(--color-primary)] px-4 py-2 rounded-md text-sm font-bold text-white">
-              Trade
-            </button>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+    return (
+        <div>
+            <h2 className="text-lg font-semibold mb-4">Results</h2>
+            <ul className="space-y-4">
+                {skills.map((skill, index) => (
+                    <li
+                        key={index}
+                        className="flex justify-between items-center dark:hover:bg-[var(--color-secondary-dark)] hover:bg-[var(--color-secondary)] transition-colors duration-200 p-4 rounded-xl"
+                    >
+                        <div className="flex items-center justify-center">
+                            <img
+                                src="https://picsum.photos/200"
+                                className="h-16 w-16 rounded-full mr-6"
+                            />
+                            <div>
+                                <div className="font-semibold">
+                                    {skill.title}
+                                </div>
+                                <div className="text-sm text-gray-400">
+                                    {skill.description}
+                                </div>
+                            </div>
+                        </div>
+                        <button
+                            className="bg-[var(--color-primary)] px-4 py-2 rounded-md text-sm font-bold text-white"
+                            onClick={() => navigate("/skill/" + index)}
+                        >
+                            Trade
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 };
 
 export default Skills;
